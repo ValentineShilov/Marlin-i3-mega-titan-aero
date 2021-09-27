58b170a4902dc5c333073e419688955cd50dc640
ValentineShilov <48096756+ValentineShilov@users.noreply.github.com>
2021-09-27 11:40:26 +0300

    buffer size

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1788,7 +1788,7 @@
 
     // The ASCII buffer for serial input
     #define MAX_CMD_SIZE 128
-    #define BUFSIZE 8
+    #define BUFSIZE 16
 
     // Transmission to Host Buffer Size
     // To save 386 bytes of PROGMEM (and TX_BUFFER_SIZE+3 bytes of RAM) set to 0.
@@ -1797,7 +1797,7 @@
     // For debug-echo: 128 bytes for the optimal speed.
     // Other output doesn't need to be that speedy.
     // :[0, 2, 4, 8, 16, 32, 64, 128, 256]
-    #define TX_BUFFER_SIZE 4 //ValentineS
+    #define TX_BUFFER_SIZE 32 //ValentineS //8
 
     // Host Receive Buffer Size
     // Without XON/XOFF flow control (see SERIAL_XON_XOFF below) 32 bytes should be enough.
